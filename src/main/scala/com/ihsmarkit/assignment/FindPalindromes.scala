d5e5550778a326580a7b1f8f877f35205ca60528
sheng88 <32003051+sheng88@users.noreply.github.com>
2017-09-24 20:09:04 -0400

    Update FindPalindromes.scala

--- src/main/scala/com/ihsmarkit/assignment/FindPalindromes.scala
+++ src/main/scala/com/ihsmarkit/assignment/FindPalindromes.scala
@@ -3,7 +3,11 @@ package com.ihsmarkit.assignment
 import scala.collection.immutable
 
 /**
-  * This object class provides methods to find all palindromes of a given string.
+  * This object class provides methods to find all palindromes in a give string.
+  * If you have sbt installed on your computer, the simplest way to run this class is navigate to the workspace
+  * directory of this project. Then execute sbt to start the sbt command prompt. At the sbt command prompt, execute
+  * run followed by the target string such as the following:
+  * > run AABBC
   */
 object FindPalindromes {
 
@@ -14,28 +18,28 @@ object FindPalindromes {
   }
 
   /*
-  This public method just prints a list of palindromes, including the palindrome's starting index in the string and the length of the 
-  string.
+  This public method returns a string representation of all the palindromes found in a given string.
    */
   def printPalindromes(palindromes:immutable.List[(String, Int)]):Unit = {
     palindromes foreach (x => println(f"${x._1},${x._2},${x._1.length}"))
   }
 
   /*
-  This public method returns a list of all palindromes found in a given string.
-  The string value in the list's tuple represents a palindrome. It's related integer value is the palindrome's starting location in the string.
+  This public method returns a List[(String, Int)] of all palindromes found in a given string.
+  Data structure of the List element is tuple (String, Int) where by String is the found palindrome and
+  Int is the starting position of the palindrome.
    */
   def getPalindromes(str: String): immutable.List[(String, Int)] = {
     val result = new scala.collection.mutable.ListBuffer[(String, Int)]()
 
     //Quick exit, return empty List
-    if (null == str || str.isEmpty || str.length == 1) return result.toList
+    if (null == str || str.length == 1) return result.toList
 
     val strLength = str.length
     for (i <- 0 to strLength) {
       for (j <- (i+1) to (strLength)) {
         val subStr = str.substring(i, j)
-        if (isPalindrome(subStr.toLowerCase())) {
+        if (isPalindrome(subStr)) {
           result += ((subStr, i))
         }
       }
@@ -49,8 +53,8 @@ object FindPalindromes {
   private def isPalindrome(str: String): Boolean = {
     //Quick exit
     if (null == str || str.isEmpty || str.length == 1) return false
-
+    val tempStr = str.toLowerCase()
     //First, obtain a list of boolean results. Then apply AND operation on the list of boolean values.
-    (for (i <- 0 to str.length / 2) yield (str(i) == str(str.length - i - 1))).reduceLeft((i, j) => i && j)
+    (for (i <- 0 to tempStr.length / 2) yield (tempStr(i) == tempStr(tempStr.length - i - 1))).reduceLeft((i, j) => i && j)
   }
 }
